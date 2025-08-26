f474d526c9c98b31036d0bd897d440b4ead7e62f
Amanda Lowe <91862988+starfluffy@users.noreply.github.com>
2025-08-27 10:26:06 +1200

    Feat/home-scene (#7)
    * feat: create first button UI
    
    * feat: home text
    
    * feat: create buttons
    
    * feat: add onclick behaviour for play button
    
    * fix: remove build
    
    * feat: floating effect

---
+++ Assets/PlayButton.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayButton : MonoBehaviour
+{
+    public Button yourButton;
+
+    void Start()
+    {
+        Button btn = yourButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        Debug.Log("You have clicked the button!");
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
