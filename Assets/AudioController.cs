05ecf8e18fa40421b4ab863a0c9fe54c889c0b9e
Charles Leng-uch <clen678@aucklanduni.ac.nz>
2025-09-02 18:05:26 +1200

    Adding temp audio file and audio functionality with volume control in inspector

---
+++ Assets/AudioController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour
+{
+    private GameAudioUi gameAudioUi;
+
+    [Header("Master Audio")]
+    [Tooltip("Master volume level (0 - 100)")]
+    public int volume = 30; // volume default set to 30% to not kill ears (0 - 100)
+
+    [Header("Audio Sources")]
+    public AudioSource backgroundMusicSource;
+    public AudioSource SFXSource;
+
+    [Header("Audio Clips")]
+    public AudioClip background;
+    public AudioClip SFX1;
+    public AudioClip SFX2;
+    public AudioClip SFX3;
+
+    private void Start() {
+        backgroundMusicSource.clip = background;
+        SetVolume(volume);
+        backgroundMusicSource.loop = true;
+        backgroundMusicSource.Play();
+
+        gameAudioUi = FindObjectOfType<GameAudioUi>();
+        gameAudioUi.UpdateAudioText(backgroundMusicSource.clip.name);
+    }
+
+    public void SetVolume(int volume)
+    {
+        float normalized = Mathf.Clamp(volume, 0, 100) / 100f;
+        backgroundMusicSource.volume = normalized;
+        SFXSource.volume = normalized;
+    }
+}
