619cb6dfa8c8df9290c68fb3aeec5bd7a3225a2b
starfluffy <amandalowe868@gmail.com>
2025-09-10 01:03:54 +1200

    save weight on input

---
+++ Assets/WeightInput.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;        // Needed for InputField
+using TMPro;                // Needed if using TextMesh Pro
+
+public class WeightInput : MonoBehaviour
+{
+    public TMP_InputField weightInputField;
+
+    void Start()
+    {
+        // preload saved weight into the input field
+        weightInputField.text = WeightController.Instance.GetWeight();
+
+        // attach SaveWeight to OnEndEdit
+        weightInputField.onEndEdit.AddListener(delegate { SaveWeight(); });
+    }
+
+    void SaveWeight()
+    {
+        WeightController.Instance.SetWeight(weightInputField.text);
+    }
+
+    public string GetSavedWeight()
+    {
+        return PlayerPrefs.GetString("PlayerWeight", "0");
+    }
+}
