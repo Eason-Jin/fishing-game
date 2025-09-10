86bd8b00bd1280704520f4c3b8836599f6161575
starfluffy <amandalowe868@gmail.com>
2025-09-10 14:30:19 +1200

    add error handling for no weight input

---
+++ Assets/WeightChecker.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class WeightChecker : MonoBehaviour
+{
+    public GameObject warningPanel;   // Group of UI components
+
+    void Start()
+    {
+        CheckWeight();
+    }
+
+    void Update()
+    {
+        //CheckWeight(); // optional, remove if not needed every frame
+    }
+
+    void CheckWeight()
+    {
+        string weight = WeightController.Instance.GetWeight();
+
+        if (string.IsNullOrEmpty(weight))
+        {
+            warningPanel.SetActive(true);
+        }
+        else
+        {
+            warningPanel.SetActive(false);
+        }
+    }
+}
