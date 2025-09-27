d990d71da760a1f1ef953460bc5d886d17cf9b09
steven11-cmd <jyao347@aucklanduni.ac.nz>
2025-09-27 19:47:00 +1200

    Music UI
    Updating function of of music ui

---
+++ Assets/MusicButton.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MusicButton : MonoBehaviour
+{
+    [SerializeField] Button musicButton;
+    [SerializeField] bool musicEnabled = false;
+    [SerializeField] AudioClip musciClip;
+    [SerializeField] AudioSource au;
+    [SerializeField] TextMeshProUGUI buttonText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        musicButton.onClick.AddListener(OnButtonClick);
+        au.volume = int.Parse(SettingsController.Instance.GetVolume());
+    }
+
+    void OnButtonClick()
+    {
+        if(!musicEnabled)
+        {
+            musicEnabled = true;
+            au.Play();
+            buttonText.text = au.clip.name;
+            
+        }
+        else
+        {
+            musicEnabled = false;
+            au.Pause();
+            buttonText.text = "Music";
+        }
+    }
+}
