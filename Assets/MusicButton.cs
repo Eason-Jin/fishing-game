e005883c7fa48275abb051747be1099be1cf0bd7
steven11-cmd <jyao347@aucklanduni.ac.nz>
2025-09-28 19:15:53 +1300

    Volume bugfix

--- Assets/MusicButton.cs
+++ Assets/MusicButton.cs
@@ -16,17 +16,17 @@ public class MusicButton : MonoBehaviour
     void Start()
     {
         musicButton.onClick.AddListener(OnButtonClick);
-        au.volume = int.Parse(SettingsController.Instance.GetVolume());
+        au.volume = float.Parse(SettingsController.Instance.GetVolume()) * 0.01f;
     }
 
     void OnButtonClick()
     {
-        if(!musicEnabled)
+        if (!musicEnabled)
         {
             musicEnabled = true;
             au.Play();
             buttonText.text = au.clip.name;
-            
+
         }
         else
         {
