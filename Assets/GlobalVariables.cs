a3574457856d18c13dfea9753429637e6920d2a0
Eason <ejin458@aucklanduni.ac.nz>
2025-10-10 12:55:56 +1300

    change log

--- Assets/GlobalVariables.cs
+++ Assets/GlobalVariables.cs
@@ -1,6 +1,6 @@
 public static class GlobalVariables
 {
     public static bool settingsComplete = false;
-    // Delay: 0.0f, Normal: 1.0f
-    public static float delay = 1.0f;
+    // Offset: 0.0f, Normal: 1.0f
+    public static float delay = 0.0f;
 }
